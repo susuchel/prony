f3903fb3931105eda537bcbb431110304a782336
oubush <oleg.u.bushuev@gmail.com>
2014-05-31 20:41:12 +0600

    Update avener
    Now it calculates average energy of each column of a matrix

--- avener.m
+++ avener.m
@@ -1,8 +1,12 @@
 function y = avener( x )
 %avener Average energy of a signal   
 %   avener(x) finds average energy of a signal x
+%   if x is a matrix, avener calculates average energy of each column
 
-y=sum(x.*x)/length(x);
+y=zeros(1,size(x,2));
+for k=1:size(x,2)
+    y(1,k)=sum(x(:,k).*x(:,k))/size(x,1);
+end
 
 
 end
