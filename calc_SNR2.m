f8ed422dd4931e8f2c3fbaaf3cd6bf1b416ae3bf
oubush <oleg.u.bushuev@gmail.com>
2014-06-04 22:37:35 +0600

    Update calc_SNR2
    Added error if sig<=0
    Now SNR is calculated for each obsravtion
    Added condition that SNR>=0

--- calc_SNR2.m
+++ calc_SNR2.m
@@ -3,9 +3,15 @@ function SNR = calc_SNR2 (signal,sig)
 %   Uses estimate of std of noise
 %
 %   Inputs:
-%   signal  - signal to analyze
+%   signal  - signal to analyze, may be a matrix, where each column is an 
+%   observation 
 %   sig     - std of noise, or its unbiased estimate in case of a real signal
-SNR = mean(10*log10(sum(signal.*signal)/(sig*sig*size(signal,1))-1));
+%   SNR = mean(10*log10(sum(signal.*signal)/(sig*sig*size(signal,1))-1));
+if (sig<=0)
+    error('calc_SNR2 (signal,sig): ''sig'' must be positive')
+end
+SNR = 10*log10(sum(signal.*signal)/(sig*sig*size(signal,1))-1);
+SNR(real(SNR)<0)=0;
 end
 
  
