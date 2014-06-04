9aa912a02d96d2002e399c6c1151c19491ef6986
oubush <oleg.u.bushuev@gmail.com>
2014-06-04 12:33:20 +0600

    Update model1
    Fixed sig
    Added defaults
    Renamed N_impl to N_obs

--- model1.m
+++ model1.m
@@ -1,6 +1,6 @@
-function [mod,sig] = model1 (fs, f0, alfa, N_mod,SNR, N_impl)
+function [mod,sig] = model1 (fs, f0, alfa, N_mod,SNR, N_obs)
 %model1 Generates a model signal 
-%   model1 (fs, f0, alfa, N_mod,SNR, N_impl) generates a model 
+%   model1 (fs, f0, alfa, N_mod,SNR, N_obs) generates a model 
 %   that is a sum of sinusoidal components. Output is N_mod samples.
 %   
 %   Input arguments:
@@ -13,13 +13,33 @@ function [mod,sig] = model1 (fs, f0, alfa, N_mod,SNR, N_impl)
 %   SNR=10*log10(sum(s[n]^2)/(N*sig^2)), s[n] - samples of a signal without
 %   noise, sig - std of noise
 %   if SNR = 0 then model without noise will be generated
-%   N_impl - number of model implementations, works if SNR!=0
+%   N_impl - number of obseravtions, works if SNR!=0
+%
+%   If some of the inputs are [], then default values will be used
 %   
 %   In this model1 magnitudes of the components are same and equal to 0.0249
 %   Also initial phases of the components are same and equal to zero
 %
-%   Each column of the result is an implementation, number of rows=number
+%   Each column of the result is an observation, number of rows=number
 %   of samples
+
+
+% define default values, works if input argument is empty
+if isempty(fs)
+    fs=500000;
+end
+
+if isempty(f0)
+    f0=[13853, 17879, 23451, 33890, 44310];
+end
+
+if isempty(alfa)
+    alfa=[610,610,610,610,610];
+end
+
+if isempty(N_mod)
+    N_mod=2500;
+end
 
 freq_op=f0;
 a_op=alfa;
@@ -36,12 +56,13 @@ for k=1:n_freq
     mods=mods+A_mod(1,k)*(exp(-a_op(k)*t_mod).*cos(2*pi*freq_op(k)*t_mod+fi_mod(1,k)));
 end
 
-mod=zeros(N_impl,N_mod); % corresponds to the model with noise
+mod=zeros(N_obs,N_mod); % corresponds to the model with noise
+sig=0;
 if (SNR==0) 
     mod=mods;
 else
-    for k=1:N_impl
-        sig=sqrt((sum(mods.^2))/(N_mod*10^(SNR/10))); % STD of noise, depending on SNR
+    sig=sqrt((sum(mods.^2))/(N_mod*10^(SNR/10))); % STD of noise, depending on SNR
+    for k=1:N_obs
         mod(k,:)=mods(1,:)+sig*randn(1,N_mod);
     end
 end
