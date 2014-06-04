d37d8fa393207f6bf5e1a3f11695bb4f6fcbf034
oubush <oleg.u.bushuev@gmail.com>
2014-06-05 02:21:33 +0600

    Update SProny
    Changed description

--- SProny.m
+++ SProny.m
@@ -6,24 +6,24 @@ function [freqs,alphas] = SProny(signal, fs, p, N_samp,fig_mode, hist_mode, file
 %   
 %   Inputs:
 %   signal      - signal to analyze, may be a martix, each column is an 
-%               implementation, number of raws = length of the signal 
+%               observation, number of raws = length of the signal 
 %   fs          - sampling frequency
 %   p           - model order, expected number of exponentials (for real signals 
 %               should be 2*n, where n - number of sinusoidals)
 %   N_samp      - number of samples to use for estimation
 %   fig_mode    - to plot or not to plot roots of polynomial, 
-%               if fig_mode~=0, then function plots roots on figure(10*i_impl+1)
+%               if fig_mode~=0, then function plots roots on figure(10*i_obs+1)
 %               roots are plotted for each segment, number of roots is equal to p
 %               all roots of all segments are plotted on one figure 
 %   hist_mode   - to plot or not to plot histogram of frequencies
-%               if hist_mode~=0, then function plots freqs of each impl on 
-%               figure(10*i_impl+2)
+%               if hist_mode~=0, then function plots freqs of each observ on 
+%               figure(10*i_obs+2)
 %   filename    - name of file to save freqs, if filename=0, then not saved 
 %   
 %   Outputs:
 %   freqs   - estimated frequencies, size of freqs is L x N x M, 
 %   where L=N_samp stands for number of segments, N=p/2 is number of 
-%   frquencies, M=size(signal,2) is number of implementations  
+%   frequencies, M=size(signal,2) is number of observations  
 %   alphas  - estimates damping factors
 %
 %   The function estimates frequencies within a segment of N_samp length,
@@ -32,19 +32,19 @@ function [freqs,alphas] = SProny(signal, fs, p, N_samp,fig_mode, hist_mode, file
 
 
 N_segm=size(signal,1)-N_samp+1; % number of segments
-N_impl=size(signal,2); % number of impementations
+N_obs=size(signal,2); % number of observations
 
-freqs=zeros(N_segm,p/2,N_impl); % for each segment p/2 estimates 
+freqs=zeros(N_segm,p/2,N_obs); % for each segment p/2 estimates 
                                % should be obtained
-alphas=zeros(N_segm,p/2,N_impl); % for each segment p/2 estimates 
+alphas=zeros(N_segm,p/2,N_obs); % for each segment p/2 estimates 
                                % should be obtained
                             
 
                                       
-for i_impl=1:N_impl
+for i_obs=1:N_obs
     
     if fig_mode~=0  %creates figure to plot roots 
-        figure(10*i_impl+1);
+        figure(10*i_obs+1);
         hold on
         x=-2:0.00001:2; y=0*x; plot(x,y) % axes
         y=-2:0.00001:2; x=0*y; plot(x,y) 
@@ -59,7 +59,7 @@ for i_impl=1:N_impl
     for i_segm=1:N_segm    
         S=zeros(N_samp,1);  %segment to analyze 
         for k=1:N_samp      %loop to obtain segment S
-            S(k,1)=signal(k+i_segm-1,i_impl); 
+            S(k,1)=signal(k+i_segm-1,i_obs); 
         end
         %SNR_S(i_segm,1)=10*log10(sum(S.*S)/(sig*sig*N_samp));
     
@@ -86,11 +86,11 @@ for i_impl=1:N_impl
         for ii=1:length(r)
            if imag(r(ii))>=0     
                 if atan(imag(r(ii))/real(r(ii)))/(2*pi*1/fs)>=0
-                    freqs(i_segm,kk,i_impl)=atan(imag(r(ii))/real(r(ii)))/(2*pi*1/fs);
-                    alphas(i_segm,kk,i_impl)=log(sqrt(imag(r(ii))*imag(r(ii))+real(r(ii))*real(r(ii))))/(1/fs);
+                    freqs(i_segm,kk,i_obs)=atan(imag(r(ii))/real(r(ii)))/(2*pi*1/fs);
+                    alphas(i_segm,kk,i_obs)=log(sqrt(imag(r(ii))*imag(r(ii))+real(r(ii))*real(r(ii))))/(1/fs);
                 else
-                    freqs(i_segm,kk,i_impl)=fs/2+atan(imag(r(ii))/real(r(ii)))/(2*pi*1/fs);
-                    alphas(i_segm,kk,i_impl)=log(sqrt(imag(r(ii))*imag(r(ii))+real(r(ii))*real(r(ii))))/(1/fs);
+                    freqs(i_segm,kk,i_obs)=fs/2+atan(imag(r(ii))/real(r(ii)))/(2*pi*1/fs);
+                    alphas(i_segm,kk,i_obs)=log(sqrt(imag(r(ii))*imag(r(ii))+real(r(ii))*real(r(ii))))/(1/fs);
                 end
            kk=kk+1;
            end
@@ -98,15 +98,15 @@ for i_impl=1:N_impl
     end
     
     if hist_mode~=0 
-        figure(10*i_impl+2);
-        hist(freqs(:,:,i_impl), 1:50:50000);
+        figure(10*i_obs+2);
+        hist(freqs(:,:,i_obs), 1:50:50000);
     end
     
 end
 
 if filename~=0
-    for i_impl=1:N_impl
-    xlswrite(filename, freqs(:,:,i_impl),i_impl); % saves each implem in a sheet
+    for i_obs=1:N_obs
+    xlswrite(filename, freqs(:,:,i_obs),i_obs); % saves each observ. in a sheet
     end
 end
 
