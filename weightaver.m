4cc8cb4f9382ee4e54d0275c92e74e7031b17d8e
oubush <oleg.u.bushuev@gmail.com>
2014-06-06 01:05:51 +0600

    Added freq limits

--- weightaver.m
+++ weightaver.m
@@ -1,7 +1,20 @@
-function f_est = weightaver( freqs,segSNR )
+function f_est = weightaver( freqs,segSNR,f2,f1 )
 %weightaver Takes weighted average of f estimate in MProny
 %   Detailed explanation goes here
 
-f_est=sum(freqs.*segSNR)/sum(segSNR);
+% TODO: Add deleting zero frequencies, complete this function
+% TODO: Create new function to estimate error for ployharmnic  signal 
+kk=1;
+freqs1=0;
+segSNR1=0;
+for k=1:length(freqs)
+    %if freqs(k)~=0
+    if ((freqs(k)<f2)&&(freqs(k)>f1))
+        freqs1(kk)=freqs(k);
+        segSNR1(kk)=segSNR(k);
+        kk=kk+1;
+    end
+end
+f_est=sum(freqs1.*segSNR1)/sum(segSNR1);
 end
 
