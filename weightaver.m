bfd28c12085660e98ce4914cf7452d9f45079dce
oubush <oleg.u.bushuev@gmail.com>
2014-06-14 01:08:15 +0600

    Add output f_mean

--- weightaver.m
+++ weightaver.m
@@ -1,4 +1,4 @@
-function f_est= weightaver(freqs,segSNR,SNRlevel)
+function [f_est, f_mean]= weightaver(freqs,segSNR,SNRlevel)
 %weightaver Takes weighted average of f estimate in MProny
 %   TODO: Detailed explanation goes here
 %   freqs should be one column
@@ -8,11 +8,14 @@ function f_est= weightaver(freqs,segSNR,SNRlevel)
 %   change result, because fi*0=0
 
 f_est=[];
+f_mean=[];
 if (SNRlevel~=0)
     segSNR(segSNR<SNRlevel)=0;
 end
 for k=1:length(segSNR)
     f_est(k,1)=sum(freqs(1:k,1).*segSNR(1:k,1))/sum(segSNR(1:k,1));
+    good=segSNR(1:k,1)>=SNRlevel;
+    f_mean(k,1)=mean(freqs(good,1));
 end
 end
 
